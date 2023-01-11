1ffe1dd7071b3d5a9aecbb1e729714959ec91405
Rio Arjuna <ri0arjuna@programmer.net>
2023-01-11 13:35:43 +0700

    handle no internet connection on CasterBottomSheet

--- app/src/main/java/com/example/movee/feature/caster/CasterBottomSheet.kt
+++ app/src/main/java/com/example/movee/feature/caster/CasterBottomSheet.kt
@@ -8,26 +8,28 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movee.base.BaseBottomSheet
 import com.example.movee.data.repository.caster.CasterResponse
 import com.example.movee.databinding.BottomSheetCasterBinding
 import com.example.movee.databinding.LoadingBinding
 import com.example.movee.network.ViewState
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class CasterBottomSheet(private val movieId: Int) : BottomSheetDialogFragment() {
+class CasterBottomSheet(private val movieId: Int) : BaseBottomSheet() {
 
     private val viewModel by viewModel<CasterViewModel>()
     private lateinit var binding: BottomSheetCasterBinding
     private lateinit var progressBinding: LoadingBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
+    override fun getLayoutView(inflater: LayoutInflater, container: ViewGroup?): View {
         binding = BottomSheetCasterBinding.inflate(inflater, container, false)
         progressBinding = LoadingBinding.bind(binding.root)
         return binding.root
+    }
+
+    override fun networkListener(state: Boolean) {
+        validateConnection(state)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,10 +49,12 @@ class CasterBottomSheet(private val movieId: Int) : BottomSheetDialogFragment() 
                         is ViewState.Loading -> {
                             progressBinding.progressBar.visibility = View.VISIBLE
                         }
+
                         is ViewState.Success -> {
                             progressBinding.progressBar.visibility = View.GONE
                             setupCasterUi(state.data)
                         }
+
                         is ViewState.Error -> {
                             progressBinding.progressBar.visibility = View.GONE
                         }
