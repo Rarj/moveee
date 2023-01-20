e720fe354beca296314c9eb82b0aa4d58ffc5b05
Rio Arjuna <ri0arjuna@programmer.net>
2023-01-20 22:28:19 +0700

    create injector module

--- features/home/src/main/java/com/example/home/HomeActivity.kt
+++ features/home/src/main/java/com/example/home/HomeActivity.kt
@@ -3,10 +3,12 @@ package com.example.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.home.databinding.ActivityHomeBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private val viewModel by viewModel<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
