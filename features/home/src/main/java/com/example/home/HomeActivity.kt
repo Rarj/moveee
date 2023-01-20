471904c3452e81747769917f82ab78f218871d34
Rio Arjuna <ri0arjuna@programmer.net>
2023-01-20 21:24:10 +0700

    layouting activity_home.xml

--- features/home/src/main/java/com/example/home/HomeActivity.kt
+++ features/home/src/main/java/com/example/home/HomeActivity.kt
@@ -1,11 +1,23 @@
 package com.example.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.home.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+    }
+
+    private fun setupUI() = with(binding) {
+        dropDownPickGenre.setOnClickListener {  }
     }
 }
