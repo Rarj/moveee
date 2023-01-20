0c6dabf71b6725730f3c9755f064ddb9fed6ee62
Rio Arjuna <ri0arjuna@programmer.net>
2023-01-20 23:23:20 +0700

    create domain module

---
+++ domain/src/main/java/com/example/domain/repo/home/HomeRepository.kt
@@ -0,0 +1,7 @@
+package com.example.domain.repo.home
+
+import kotlinx.coroutines.flow.Flow
+
+interface HomeRepository {
+//    suspend fun discoverMovie(genreID: Int): Flow<PagingData<DiscoverMovie>>
+}
