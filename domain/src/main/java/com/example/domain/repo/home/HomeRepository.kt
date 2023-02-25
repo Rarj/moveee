7574ab38fafe59bd59f17ac6c0d49913f80412c0
Rio Arjuna <ri0arjuna@programmer.net>
2023-02-25 11:55:08 +0700

    remove implementation of KoinComponent oh HomeRepository.kt

--- domain/src/main/java/com/example/domain/repo/home/HomeRepository.kt
+++ domain/src/main/java/com/example/domain/repo/home/HomeRepository.kt
@@ -2,8 +2,7 @@ package com.example.domain.repo.home
 
 import com.example.domain.repo.response.DiscoverMovie
 import kotlinx.coroutines.flow.Flow
-import org.koin.core.component.KoinComponent
 
-interface HomeRepository: KoinComponent {
+interface HomeRepository {
     suspend fun discoverMovie(genreID: Int): Flow<DiscoverMovie>
 }
